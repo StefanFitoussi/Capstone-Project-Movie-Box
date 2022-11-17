0404d41bbaf21b52514abdafe679cc1cce5493f8
Stefan Fitoussi <stefan.fit@web.de>
2022-11-17 11:16:58 +0100

    fetch and render movies list

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,31 @@
 import styled from "styled-components";
-import { getAllMovies } from "../helpers/api";
+import movieData from "../db.json";
 
 export default function Home() {
   return (
-    <>
+    <main>
       <h1>Your Movies</h1>
-    </>
+      <br />
+      {movieData &&
+        movieData.map((movie) => {
+          return (
+            <StyledUl key={movie.id}>
+              <StyledLi>
+                <strong>{movie.original_title}</strong>
+              </StyledLi>
+              <StyledLi>Veröffentlichungsdatum: {movie.release_date}</StyledLi>
+              <StyledLi>Laufzeit: {movie.runtime} Min.</StyledLi>
+            </StyledUl>
+          );
+        })}
+    </main>
   );
 }
+
+const StyledUl = styled.ul`
+  list-style-type: none;
+`;
+
+const StyledLi = styled.li`
+  list-style-type: none;
+`;
