c5dabb716e24971b66813fc0f2150f53346ec994
Stefan Fitoussi <stefan.fit@web.de>
2022-11-21 11:13:32 +0100

    delete background

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import MovieCard from "../components/MovieCard";
 export default function Home() {
   return (
     <>
-      <Background />
       <StyledH2>Choose your Movies:</StyledH2>
       <MovieCard />
     </>
@@ -13,14 +12,4 @@ export default function Home() {
 
 const StyledH2 = styled.h2`
   margin-top: 35px;
-`;
-
-const Background = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  overflow-y: hidden;
-  top: 0;
-  left: 0;
-  z-index: -10;
 `;
